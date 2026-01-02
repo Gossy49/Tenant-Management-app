13a9ce53a1673d9befbf958373f22c93a44988a7
God'stime Okolo <godstimeokolo25@gmail.com>
2026-01-02 13:16:49 +0100

    Add tenants data structure

---
+++ App.py
@@ -0,0 +1,26 @@
+tenants = [
+
+    {
+    "name" :'Jack Brown',
+    "apartment_number": 1,
+    "apartment_type": "1 Bedroom flat",
+    "rent_amount": 350000,
+    "amount_paid": 20000,
+    "due_date" : "20th June 2026",},
+
+    {"name" :'Mary Jay',
+    "apartment_number": 2,
+    "apartment_type": "1 Bedroom flat",
+    "rent_amount": 350000,
+    "amount_paid": 25000,
+    "due_date" : "20th June 2026",},
+
+    {"name" :"Glory flares",
+    "apartment_number": 3,
+    "apartment_type": "1 Bedroom flat",
+    "rent_amount": 350000,
+    "amount_paid": 10000,
+    "due_date" : "20th June 2026",},
+
+]
+
